9eed8ae1f57011872e9bd79c56481f3d1824464b
youngmentor <olaleresuliton43@gmail.com>
2023-07-05 13:53:06 +0100

    made chANGES

--- src/component/LandingPage/Home.tsx
+++ src/component/LandingPage/Home.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import './Home.css'
 import Header from '../Header/Header';
 import Popular from './Popular/PopularHotel';
 import AllRooms from './AllRooms/AllRooms';
-import Landing1 from './Landing1/Landing1';
 const Home: React.FC = () => {
-
-  const navigate = useNavigate()
   return (
     <div className='HomeMainPage'>
       <Header />
       <div className='HomeMainPageWrap'>
         <AllRooms />
         <Popular />
-        <div className='HomeBttn'>
-          <button onClick={() => navigate("/login")}>login</button>
-          <button onClick={() => navigate("/signup")}>sign up</button>
-        </div>
       </div>
     </div>
   );
