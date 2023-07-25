dd686cad160666e0b40fcdadfdf57e2d9c80fcfe
youngmentor <olaleresuliton43@gmail.com>
2023-07-25 22:26:26 +0100

    new commit

--- src/Component2/AdminDash/AdminDashLeft/AdminDashLeft.tsx
+++ src/Component2/AdminDash/AdminDashLeft/AdminDashLeft.tsx
@@ -5,38 +5,39 @@ import { RxDashboard } from "react-icons/rx";
 import { FaHotel } from "react-icons/fa";
 import { MdOutlineBedroomParent } from "react-icons/md";
 import { MdAddHome } from "react-icons/md";
+import HomeLogo from './RoomLogo-removebg-preview.png'
 const AdminDashLeft: React.FC = () => {
 
     const navigate = useNavigate()
     return (
         <div className='AdminDashLeftMain'>
             <div className='AdminDashLeftMainWrap'>
-                {/* <div className='AdminDashboardUserImgDiv'>
-                    <img src={User} alt='userimg'  className='UserImg'/>
-                </div> */}
+                <div className='AdminDashboardUserImgDiv'>
+                    <img src={HomeLogo} alt='userimg' className='AdminHome_Logo' />
+                </div>
                 <div className='AdminDashBoardLeftNav'>
                     <div className='AdminDashBoardLeftNav_Icon_Div'>
                         <RxDashboard />
                         <p onClick={() => navigate("/admindash/dashmain")}>DashBoard</p>
                     </div>
                     <div className='AdminDashBoardLeftNav_Icon_Div'>
-                        <FaHotel/>
+                        <FaHotel />
                         <p onClick={() => navigate("/admindash/allhotels")}>All Hotels</p>
                     </div>
                     <div className='AdminDashBoardLeftNav_Icon_Div'>
-                        <MdAddHome/>
+                        <MdAddHome />
                         <p onClick={() => navigate("/admindash/addhotels")}>Add Hotels</p>
                     </div>
                     <div className='AdminDashBoardLeftNav_Icon_Div'>
-                        <MdOutlineBedroomParent/>
+                        <MdOutlineBedroomParent />
                         <p onClick={() => navigate("/admindash/allrooms")}>All Rooms</p>
                     </div>
                     <div className='AdminDashBoardLeftNav_Icon_Div'>
-                        <MdAddHome/>
+                        <MdAddHome />
                         <p onClick={() => navigate("/admindash/addrooms")}>Add Rooms</p>
                     </div>
                     <div className='AdminDashBoardLeftNav_Icon_Div'>
-                        <MdAddHome/>
+                        <MdAddHome />
                         <p onClick={() => navigate("/admindash/addfacility")}>Add Facility</p>
                     </div>
                 </div>
